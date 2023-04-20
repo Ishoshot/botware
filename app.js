3b5913aefcc8f8339972d10b93e1ad5d9297af17
ishoshot <ishoshot@gmail.com>
2023-04-20 16:35:03 +0100

    Fix for Empty Account Number

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.post('/api/enquiry/balance', async (req, res) => {
   if (JSON.parse(params).AccountNumber == '') {
     return res.status(200).json({
       code: '09',
-      message: 'Sorry! I believe you are trying to get the balance of an account. Please provide the account number.',
+      message: 'Sorry! I believe you are trying to get the balance of an account. Kindly rephrase your request and ensure an account number is provided.',
       otherMessage: ''
     });
   }
